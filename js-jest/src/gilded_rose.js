ab07f4a5c8ef3009054f05fb1b6c7185bb93bfa0
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-13 11:18:38 +0100

    move aged brie to its own special case

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -27,28 +27,25 @@ class Shop {
                 }
                 item.sellIn = item.sellIn - 1;
                 continue;
-            }
-            if (item.name != "Aged Brie") {
+            } else if (item.name == "Aged Brie") {
+                if (item.sellIn <= 0) {
+                    item.quality = item.quality + 2;
+                } else {
+                    item.quality = item.quality + 1;
+                }
+            } else {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-            }
-            item.sellIn = item.sellIn - 1;
-            if (item.sellIn < 0) {
-                if (item.name != "Aged Brie") {
+                item.sellIn = item.sellIn - 1;
+                if (item.sellIn < 0) {
                     if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
                 }
             }
+            if (item.quality < 0) item.quality = 0;
+            if (item.quality > 50) item.quality = 50;
         }
 
         return this.items;

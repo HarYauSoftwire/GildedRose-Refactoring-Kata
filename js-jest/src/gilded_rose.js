5386a662725f604020e95f23e0d6716c4c2b5180
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-13 11:07:00 +0100

    move backstage passes to its own special case

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -15,43 +15,33 @@ class Shop {
             if (item.name == "Sulfuras, Hand of Ragnaros") {
                 continue;
             }
-            if (
-                item.name != "Aged Brie" &&
-                item.name != "Backstage passes to a TAFKAL80ETC concert"
-            ) {
+            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.sellIn <= 0) {
+                    item.quality = 0;
+                } else if (item.sellIn <= 5) {
+                    item.quality += 3;
+                } else if (item.sellIn <= 10) {
+                    item.quality += 2;
+                } else {
+                    item.quality += 1;
+                }
+                item.sellIn = item.sellIn - 1;
+                continue;
+            }
+            if (item.name != "Aged Brie") {
                 if (item.quality > 0) {
                     item.quality = item.quality - 1;
                 }
             } else {
                 if (item.quality < 50) {
                     item.quality = item.quality + 1;
-                    if (
-                        item.name == "Backstage passes to a TAFKAL80ETC concert"
-                    ) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
                 }
             }
             item.sellIn = item.sellIn - 1;
             if (item.sellIn < 0) {
                 if (item.name != "Aged Brie") {
-                    if (
-                        item.name != "Backstage passes to a TAFKAL80ETC concert"
-                    ) {
-                        if (item.quality > 0) {
-                            item.quality = item.quality - 1;
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
                     }
                 } else {
                     if (item.quality < 50) {

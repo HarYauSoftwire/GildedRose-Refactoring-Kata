7023ce5264bf5e6b4add290f1f15a7c6ef500ddf
Harvey Yau <Harvey.Yau@softwire.com>
2021-07-13 11:27:15 +0100

    more refactoring

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -15,40 +15,39 @@ class Shop {
             if (item.name == "Sulfuras, Hand of Ragnaros") {
                 continue;
             }
+
             if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                if (item.sellIn <= 0) {
-                    item.quality = 0;
-                } else if (item.sellIn <= 5) {
-                    item.quality += 3;
-                } else if (item.sellIn <= 10) {
-                    item.quality += 2;
-                } else {
-                    item.quality += 1;
-                }
-                item.sellIn = item.sellIn - 1;
-                continue;
-            } else if (item.name == "Aged Brie") {
-                if (item.sellIn <= 0) {
-                    item.quality = item.quality + 2;
-                } else {
-                    item.quality = item.quality + 1;
-                }
+                this.updateBackstagePassQuality(item);
             } else {
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
+                var qualityDecreasePerDay = 1;
+                if (item.name == "Aged Brie") {
+                    qualityDecreasePerDay *= -1;
                 }
-                item.sellIn = item.sellIn - 1;
                 if (item.sellIn < 0) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                    item.quality -= 2 * qualityDecreasePerDay;
+                } else {
+                    item.quality -= qualityDecreasePerDay;
                 }
             }
+
+            item.sellIn -= 1;
             if (item.quality < 0) item.quality = 0;
             if (item.quality > 50) item.quality = 50;
         }
 
         return this.items;
+    }
+
+    updateBackstagePassQuality(item) {
+        if (item.sellIn > 10) {
+            item.quality += 1;
+        } else if (item.sellIn > 5) {
+            item.quality += 2;
+        } else if (item.sellIn > 0) {
+            item.quality += 3;
+        } else {
+            item.quality = 0;
+        }
     }
 }
 
